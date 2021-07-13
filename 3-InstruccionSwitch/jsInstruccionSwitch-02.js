725c23a5478a6cda5f6d45f5f5c09451df5b7203
MartinFaraguna <faraguna.martin@gmail.com>
2021-07-13 20:37:36 -0300

    segundo commit

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,10 +1,65 @@
-function mostrar()
-{
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+function mostrar() {
 
+	let mes;
 
+	mes = document.getElementById("txtIdMes").value;
 
+	switch (mes) {
+		case "Enero":
+		case "Febrero":
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "Junio":
+			alert("Falta para el invierno.");
+			break;
+		case "Julio":
+		case "Agosto":
+			alert("Abrigate que hace frio.");
+			break;
+		default:
+			alert("Ya pasamos el frio, ahora calor!!!.");
+	}
+	
+	// switch (mes) {
+	// 	case "Enero":
+	// 		alert("Falta para el invierno.");
+	// 		break;
+	// 	case "Febrero":
+	// 		alert("Falta para el invierno.");
+	// 		break;
+	// 	case "Marzo":
+	// 		alert("Falta para el invierno.");
+	// 		break;
+	// 	case "Abril":
+	// 		alert("Falta para el invierno.");
+	// 		break;
+	// 	case "Mayo":
+	// 		alert("Falta para el invierno.");
+	// 		break;
+	// 	case "Junio":
+	// 		alert("Falta para el invierno.");
+	// 		break;
+	// 	case "Julio":
+	// 		alert("Abrigate que hace frio.");
+	// 		break;
+	// 	case "Agosto":
+	// 		alert("Abrigate que hace frio.");
+	// 		break;
+	// 	case "Septiembre":
+	// 		alert("Ya pasamos el frio, ahora calor!!!.");
+	// 		break;
+	// 	case "Octubre":
+	// 		alert("Ya pasamos el frio, ahora calor!!!.");
+	// 		break;
+	// 	case "Noviembre":
+	// 		alert("Ya pasamos el frio, ahora calor!!!.");
+	// 		break;
+	// 	case "Diciembre":
+	// 		alert("Ya pasamos el frio, ahora calor!!!.");
+	// 		break;
+	// }
+
+	
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,22 @@
-function mostrar()
-{
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+function mostrar() {
+	let mes;
+	mes = document.getElementById("txtIdMes").value;
 
-
+	switch (mes) {
+		case "Enero":
+		case "Marzo":
+		case "Mayo":
+		case "Julio":
+		case "Agosto":
+		case "Octubre":
+		case "Diciembre":
+			alert("Este mes tiene 31 días");
+			break;
+		case "Febrero":
+			alert("Este mes tiene 28 días");
+			break;
+		default:
+			alert("Este mes tiene 30 días");
+	}
 
 }//FIN DE LA FUNCIÓN
